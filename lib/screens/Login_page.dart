88b103eb1553b3aab841d9b5f1b0042cec6d8cf7
TrinhSonTung <trinhsontung2410@gmail.com>
2024-06-08 16:45:17 +0700

    update google login button

--- lib/screens/Login_page.dart
+++ lib/screens/Login_page.dart
@@ -4,9 +4,8 @@ import 'dart:math';
 
 import 'package:destinymatch/main.dart';
 import 'package:destinymatch/services/AccountService.dart';
-import 'package:destinymatch/services/AccountService.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/rendering.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -21,24 +20,15 @@ class LoginFunction extends State<LoginPage> {
   void _login() async {
     final email = _emailController.text;
     final password = _passwordController.text;
-    final response = await _accountservice.login(email, password);
+    await _accountservice.login(email, password);
 
-    if (response.containsKey('error')) {
-      // Xử lý lỗi đăng nhập
-      print(response['error']);
-    } else {
-      // Xử lý đăng nhập thành công
-      print('Login successful: $response');
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-            builder: (context) => MyHomePage(
-                title:
-                    "hellow")), // Thay MyHomePage bằng tên widget của trang bạn muốn điều hướng đến
-      );
-
-      // Lưu token hoặc dữ liệu cần thiết khác
-    }
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+          builder: (context) => const MyHomePage(
+              title:
+                  "hello")), // Thay MyHomePage bằng tên widget của trang bạn muốn điều hướng đến
+    );
   }
 
   // @override
@@ -110,35 +100,69 @@ class LoginFunction extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images.jpg'),
-          TextField(
-            controller: _emailController,
-            decoration: InputDecoration(labelText: 'Email'),
-          ),
-          TextField(
-            controller: _passwordController,
-            decoration: InputDecoration(labelText: 'Password'),
-            obscureText: true,
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _login,
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.pink,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20), // button's shape
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images.jpg'),
+            const SizedBox(
+              height: 20,
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                  labelText: 'Email', border: OutlineInputBorder()),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                  labelText: 'Password', border: OutlineInputBorder()),
+              obscureText: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text("Forgot password?",
+                  style: TextStyle(
+                      color: Colors.blue[300],
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.blue[600])),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _login,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20), // button's shape
+                ),
+              ),
+              child: const Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.white), // Thay đổi màu chữ sang màu vàng
               ),
             ),
-            child: const Text(
-              'Login',
-              style: TextStyle(
-                  color: Colors.white), // Thay đổi màu chữ sang màu vàng
+            const SizedBox(height: 10),
+            Container(
+              width: 50,
+              height: 50,
+              padding:
+                  EdgeInsets.all(8), // Khoảng cách bên trong để cân đối logo
+              decoration: BoxDecoration(
+                color: Colors.white, // Màu nền trắng
+                borderRadius: BorderRadius.circular(8), // Bo góc
+                border: Border.all(color: Colors.grey, width: 1), // Viền
+              ),
+              child: Image.asset(
+                'assets/googleLogo.png', // Đường dẫn đến hình ảnh Google icon
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/services/AccountService.dart
+++ lib/services/AccountService.dart
@@ -1,32 +1,40 @@
 import 'dart:convert';
-import 'package:destinymatch/models/Account.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Accountservice {
   final String apiLink = "https://localhost:7016";
 
   Accountservice();
 
-  Future<Map<String, dynamic>> login(String email, String password) async {
-    final url = Uri.parse("${apiLink}/api/Accounts/login");
+  Future<void> login(String email, String password) async {
+    final url = Uri.parse("$apiLink/api/Accounts/login");
+    final storage = FlutterSecureStorage();
     final response = await http.post(
       url,
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
-      body:
-          jsonEncode({"email": email, "password": password}),
+      body: jsonEncode({"email": email, "password": password}),
     );
 
     if (response.statusCode == 200) {
-      // final storage = new FlutterSecureStorage();
-      // await storage.write(key: "account", value: response.body);
       final Map<String, dynamic> responseBody = jsonDecode(response.body);
       print(responseBody);
-      return responseBody;
+
+      // Giả sử API trả về một token trong response
+      String token = responseBody['token'];
+
+      // Lưu token vào Flutter Secure Storage
+      await storage.write(key: "token", value: token);
+
+      // Hoặc lưu trữ thông tin người dùng vào Shared Preferences
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('token', jsonEncode(responseBody['token']));
+
     } else {
-      return {"error": "Login failed"};
+      print({"error": "Login failed"});
     }
   }
 }
